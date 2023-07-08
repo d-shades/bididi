3000c7d77a48dd745459dc9a8fd443bd0c311b5e
d-shades <alfredo.becerra@asociacionaepi.es>
2023-07-08 12:50:35 +0200

    start project 2

--- bididi.py
+++ bididi.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+import pickle
 
 
 @dataclass
@@ -32,6 +33,17 @@ class SongCollection:
     def __init__(self) -> None:
         self.collection = []
 
+    def save_collection(self):
+        with open("collection.pkl", "w", encodign="utf-8") as io:
+            pickle.dump(self.collection, io)
+
+    def read_collection(self):
+        with open("collection.pkl") as io:
+            return pickle.load(io, encoding="utf-8")
+
+    def add_to_collection(self, song):
+        self.collection.append(song)
+
 
 class SongPublisher:
     """
@@ -56,3 +68,6 @@ class SongPublisher:
         artist = cls.create_artist(artist_name)
         album = cls.create_album(album_title, published_date)
         song = cls.create_song(song_title, artist, album, published_date, duration)
+        collection = SongCollection()
+        collection.add_to_collection(song)
+        collection.save_collection()
